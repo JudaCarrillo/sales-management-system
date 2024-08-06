7c27831a2c7616c76335f3ed4619afb8a64801ae
Selfxz <sebassalinasobregon@gmail.com>
2024-08-06 17:28:13 -0500

    correccion

--- src/views/Maintenance/Producto_view.php
+++ src/views/Maintenance/Producto_view.php
@@ -15,68 +15,59 @@
         <div class="dividor">
             <section class="form">
                 <h1>DETALLES DE PRODUCTO</h1>
-                <form action="" method="post">
-                    <article class="Parte1">
-                        <div class="group">
-                            <label for="codigo">Codigo</label>
-                            <input type="text" name="codigo" id="codigo">
+                <form action="../../../save_product.php"  method="post">
+                        <article class="Parte1">
+                            <div class="group">
+                                <label for="codigo">Codigo</label>
+                                <input type="text" name="code" id="code">
+                            </div>
+                            <div class="group">
+                                <label for="procedencia">Procedencia</label>
+                                <input type="text" name="source" id="source">
+                            </div>
+                        </article>
+                        <article class="Parte2">
+                            <div class="group">
+                                <label for="nombre">Nombre</label>
+                                <input type="text" name="name" id="name">
+                            </div>
+                            <div class="group">
+                                <label for="unidad">Unidad</label>
+                                <input type="text" name="unit" id="unit">
+                            </div>
+                            <div class="group">
+                                <label for="categoria">Categoría</label>
+                                <input type="text" name="category" id="category">
+                            </div>
+                            <div class="group">
+                                <label for="marca">Marca</label>
+                                <input type="text" name="brand" id="brand">
+                            </div>
+                            <div class="group">
+                                <label for="stock">Stock</label>
+                                <input type="number" name="stock" id="stock">
+                            </div>
+                            <div class="group">
+                                <label for="precio">Precio</label>
+                                <input type="number" name="price" id="price">
+                            </div>
+                            <div class="group">
+                            <label for="stock">Estado:</label>
+                            <input type="checkbox" id="status" name="status" checked="true">
+                            </div>
+                            
+                        </article>
+                        <div class="separado">
+                            <div class="group">
+                                <label for="fecha_creacion">Fecha de Creación</label>
+                                <input type="date" name="fecha_creacion" id="fecha_creacion">
+                            </div>
+                            <div class="group">
+                                <label for="fecha_actualizacion">Fecha de Actualización</label>
+                                <input type="date" name="fecha_actualizacion" id="fecha_actualizacion">
+                            </div>
                         </div>
-                        <div class="group">
-                            <label for="procedencia">Procedencia</label>
-                            <select name="procedencia" id="procedencia">
-                                <option value="">Seleccione procedencia</option>
-                            </select>
-                        </div>
-                    </article>
-                    <article class="Parte2">
-                        <div class="group">
-                            <label for="nombre">Nombre</label>
-                            <input type="text" name="nombre" id="nombre">
-                        </div>
-                        <div class="group">
-                            <label for="unidad">Unidad</label>
-                            <select name="unidad" id="unidad">
-                                <option value="">Seleccione unidad</option>
-                            </select>
-                        </div>
-                        <div class="group">
-                            <label for="categoria">Categoría</label>
-                            <select name="categoria" id="categoria">
-                                <option value="">Seleccione categoría</option>
-                            </select>
-                        </div>
-                        <div class="group">
-                            <label for="marca">Marca</label>
-                            <select name="marca" id="marca">
-                                <option value="">Seleccione marca</option>
-                            </select>
-                        </div>
-                        <div class="group">
-                            <label for="stock">Stock</label>
-                            <input type="number" name="stock" id="stock">
-                        </div>
-                        <div class="group">
-                            <label for="precio">Precio</label>
-                            <input type="number" name="precio" id="precio">
-                        </div>
-                        <div class="group">
-                            <label for="status">Status</label>
-                            <select name="status" id="status">
-                                <option value="">Seleccione status</option>
-                            </select>
-                        </div>
-                    </article>
-                    <div class="separado">
-                        <div class="group">
-                            <label for="fecha_creacion">Fecha de Creación</label>
-                            <input type="date" name="fecha_creacion" id="fecha_creacion">
-                        </div>
-                        <div class="group">
-                            <label for="fecha_actualizacion">Fecha de Actualización</label>
-                            <input type="date" name="fecha_actualizacion" id="fecha_actualizacion">
-                        </div>
-                    </div>
-                </form>
+                    </form>
 
                 
             </section>
