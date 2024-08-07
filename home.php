18752736f83c253397933808e1652c197102de49
Gabriel <josegabrielhc2@gmail.com>
2024-08-06 23:50:03 -0500

    navbar_v2

--- home.php
+++ home.php
@@ -9,14 +9,12 @@
     <link rel="stylesheet" href="./assets/css/home.css">
 </head>
 <body>
-    <header class="bg-custom text-white py-3">
         <?php include './assets/component/navbar.php'; ?>
-    </header>
     <main class="py-5">
         <h1 class="font-weight-bold">Guille Market</h1>
         <h2>Sistema de Gestión de Ventas</h2>
         <section class="hero text-center bg-primary text-white py-5">
-            <img src="./assets/img/rediseño.jpg" alt="Example Image" class="mt-4 img-fluid centered-image">
+            <img src="./assets/img/re" alt="Example Image" class="mt-4 img-fluid centered-image">
         </section>
         <!-- Contenido adicional -->
     </main>
